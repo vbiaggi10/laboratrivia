534ba7e7e75ffc315c45ccacce5131b6ba1e407f
Valeria <vbiaggi10@gmail.com>
2018-05-18 18:46:21 -0500

    arreglo en las respuestas

--- js/index.js
+++ js/index.js
@@ -10,14 +10,14 @@ function myFunction() {
   respuestasCorrectas.innerHTML = "<h4> Preguntas correctas </h4>";
   respuestasIncorrectas.innerHTML = "<h4> Preguntas incorrectas </h4>";
 
-  var primeraPregunta=Number(document.getElementById('answer1'));
+  var primeraPregunta=Number(document.getElementById('answer1').value);
   if (primeraPregunta == 2) { //Ejemplo de opción con NÚMERO
     respuestasCorrectas.innerHTML += "1.¿Cuál es la bebida preferida de Gonzalo? <br> <strong>Respuesta correcta:</strong> 🍺 La cerveza 🍺";
   } else {
     respuestasIncorrectas.innerHTML += "1.¿Cuál es la bebida preferida de Gonzalo? <br> <strong>La respuesta correcta era:</strong> 🍺 La cerveza 🍺";
   }
 
-  var segundaPregunta = document.getElementById('answer2').toString();
+  var segundaPregunta = String(document.getElementById('answer2').value);
   if (segundaPregunta == "A") { //Ejemplo de opción con LETRA
     //respuestasCorrectas.innerHTML += "<br> 2.¿Qué es lo que más le gusta hacer a Lulú? <br> <strong>Respuesta correcta:</strong> 🎤 Cantar 🎤";
     respuestasCorrectas.innerHTML += "<br> 2.¿Qué es lo que más le gusta hacer a Lulú? <br> <strong>Respuesta correcta:</strong> 🎤 Cantar 🎤";
